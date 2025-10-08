4b6e44d46c6f44206bbbefdbbf79d4ba0b48f844
Eto0-cloud <katlegolechile@gmail.com>
2025-10-08 19:41:37 +0000

    Add ChequeAccount class with withdrawal functionality

---
+++ Banking System/ChequeAccount.java
@@ -0,0 +1,10 @@
+public class ChequeAccount extends Account {
+    public ChequeAccount(double initialDeposit) {
+        super(initialDeposit);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount <= balance) balance -= amount;
+    }
+}
