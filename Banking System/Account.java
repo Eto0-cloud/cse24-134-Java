f27cc1ecceaffcba614ab53f017491db917ce9d3
Eto0-cloud <katlegolechile@gmail.com>
2025-10-08 19:37:40 +0000

    check for errors

---
+++ Banking System/Account.java
@@ -0,0 +1,23 @@
+public abstract class Account {
+    protected String accountNumber;
+    protected double balance;
+
+    public Account(double initialDeposit) {
+        this.balance = initialDeposit;
+        this.accountNumber = generateAccountNumber();
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public abstract void withdraw(double amount);
+
+    public double getBalance() {
+        return balance;
+    }
+
+    private String generateAccountNumber() {
+        return "ACCT" + Math.round(Math.random() * 100000);
+    }
+}
